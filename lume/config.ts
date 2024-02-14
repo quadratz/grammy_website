9952e737f1d8d2bb81e32af2a067b4f71cc9e8da
quadratz <quadratz@proton.me>
2024-02-14 21:23:45 +0700

    build: 👷 adjust url path to match with `vitepress`

--- lume/config.ts
+++ lume/config.ts
@@ -1,9 +1,11 @@
 import lume from "lume/mod.ts";
-import { currentVersions } from "./plugins/current_versions/mod.ts";
+import modifyUrls from "lume/plugins/modify_urls.ts";
 import { betterLineBreaks } from "./plugins/better_line_breaks/mod.ts";
+import { currentVersions } from "./plugins/current_versions/mod.ts";
 
 const site = lume({
   src: "./docs",
+  prettyUrls: false,
 }, {
   markdown: {
     plugins: [betterLineBreaks],
@@ -18,5 +20,11 @@ site.addEventListener("beforeRender", (event) => {
     }
   }
 });
+
+site.use(modifyUrls({
+  fn: (url) => {
+    return url.replace(/.html$/, "");
+  },
+}));
 
 export default site;
