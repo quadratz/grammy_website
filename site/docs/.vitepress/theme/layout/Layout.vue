fb0e0b0cbf2aaeffbb8dbaf1df6ea30e41125a25
Qz <quadratz@proton.me>
2024-03-26 10:04:37 +0700

    add timestamp

--- site/docs/.vitepress/theme/layout/Layout.vue
+++ site/docs/.vitepress/theme/layout/Layout.vue
@@ -5,10 +5,15 @@ import HomeHeroInfo from "./HomeHeroInfo.vue";
 
 const { Layout } = DefaultTheme as any;
 const { frontmatter } = useData();
+
+const timestamp = new Date().toString();
 </script>
 
 <template>
   <Layout>
+    <template #layout-top>
+      <p>Build: {{ timestamp }}</p>
+    </template>
     <template #home-hero-info>
       <HomeHeroInfo :name="frontmatter.hero.name" :text="frontmatter.hero.text" :taglines="frontmatter.hero.taglines" />
     </template>
